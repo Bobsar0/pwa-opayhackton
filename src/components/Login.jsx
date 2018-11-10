7c875d3172f90ea43a11b7886b75b1c9e1f27f16
yemistorms <horperyemi99@gmail.com>
2018-11-10 14:44:25 +0100

    Login Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
+    <div class="row login">
+        <div class="col-md-7"></div>
+          <div class="col-md-5">
+            <form class="form login-form">               
+              <span>
+                  <h3>Welcome</h3>
+              </span>
 
+              <div class="form-group">
+                  <label for="username">Username</label>
+                  <input type="text" class="form-control" required></input>
+              </div>
+
+              <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" class="form-control" required></input>
+              </div>
+
+              <button class="btn btn-succcess"></button>
+            </form>
+
+            <div class="">
+                <span>Don't have an accout? <NavLink to="/signup">Sign Up</NavLink></span>
+            </div>
+        </div>
     </div>
   );
 };
